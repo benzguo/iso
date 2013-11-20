9d8e0f10255dd7fa20b31a735aee0b02c5436109
Ben Guo <ben@venmo.com>
2013-11-19 22:20:57 -0500

    better movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,56 +2,57 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+	
 	public GameObject mainCamera;
 	public float speed = 10.0f;
-
+	
 	float cameraHeight;
 	Vector3 targetPos;
-	Vector3 lastCollisionDir = Vector3.zero;
+	//	Vector3 previousPos;
 	
 	void Start () {
 		cameraHeight = mainCamera.transform.position.y; 
 	}
-
+	
 	void Update () {
 		// get the target position from the mouse (or touch) position
 		if (Input.GetMouseButton(0)) {
 			var mousePos = Input.mousePosition;
 			mousePos.z = cameraHeight;
-
+			
 			var rMousePos = Camera.main.ScreenToWorldPoint(mousePos) - transform.position;
 			var cMousePos = cardinalize (rMousePos);
 			if (Vector3.Distance(rMousePos, Vector3.zero) >= 1) 
 			{
+				//				previousPos = targetPos;
 				targetPos = roundedVec(transform.position + cardinalize(rMousePos));
 			}
-//			Debug.Log (targetPos);
+			//			Debug.Log (targetPos);
 		}
-
+		
 		Vector3 targetDir = cardinalize (targetPos - transform.position);
-//		if (targetDir != lastCollisionDir) {
+		bool shouldMove = !Physics.Raycast (targetPos, targetDir, 0.5f);
+		if (shouldMove) {
 			transform.position = Vector3.MoveTowards (transform.position, targetPos, speed * Time.deltaTime);
-			lastCollisionDir = Vector3.zero;
-//		}
+		}
 		Debug.Log (targetPos);
-			
-
-//		transform.rotation = Quaternion.identity;
+		
+		
+		//		transform.rotation = Quaternion.identity;
 	}
-
-	void OnTriggerEnter (Collider other)
-	{
-		var collisionPos = other.gameObject.transform.position;
-		lastCollisionDir = cardinalize (collisionPos - transform.position);
-		Debug.Log ("TRIGGER");
-//		targetPos = transform.position;
-//		transform.position = new Vector3 (transform.position.x, 0, transform.position.z);
-//		if (other.gameObject.tag == "Ground") {
-//		Debug.Log (lastCollisionDir);
-//		}
-	}
-
+	
+	//	void OnTriggerEnter (Collider other)
+	//	{
+	//		var collisionPos = other.gameObject.transform.position;
+	//		lastCollisionDir = cardinalize (collisionPos - transform.position);
+	//		Debug.Log ("TRIGGER");
+	//		targetPos = transform.position;
+	//		transform.position = new Vector3 (transform.position.x, 0, transform.position.z);
+	//		if (other.gameObject.tag == "Ground") {
+	//		Debug.Log (lastCollisionDir);
+	//		}
+	//	}
+	
 	Vector3 cardinalize (Vector3 vector) {
 		Vector3 cv = Vector3.zero;
 		float ax = Mathf.Abs (vector.x);
@@ -63,7 +64,7 @@ public class PlayerController : MonoBehaviour {
 		}
 		return cv;
 	}
-
+	
 	// TODO learn about vector math
 	// TODO move this somewhere shared
 	Vector3 roundedVec (Vector3 v) {
@@ -71,5 +72,5 @@ public class PlayerController : MonoBehaviour {
 		                    0,
 		                    Mathf.RoundToInt (v.z));
 	}
-
+	
 }
