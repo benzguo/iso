deb9f99fc7b6103884e52ebc1384d3a320d6e9c9
Ben Guo <ben@venmo.com>
2013-11-23 17:43:19 -0500

    better diagonal movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,56 +2,93 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	
-	public GameObject mainCamera;
-	public float speed = 10.0f;
-	
-	float cameraHeight;
+
+	public GameObject gMainCamera;
+	public float gSpeed = 1.0f;
+	public Vector3 gNormal;
+
 	Vector3 targetPos;
-	
-	void Start () {
-		cameraHeight = mainCamera.transform.position.y; 
+	bool atTargetPos;
+
+	void Start() {
+		gNormal = Vector3.down;
+		targetPos = transform.position;
+		atTargetPos = true;
 	}
-	
-	void Update () {
+
+	void Update() {
 		// get the target position from the mouse (or touch) position
 		if (Input.GetMouseButton(0)) {
-			var mousePos = Input.mousePosition;
-			mousePos.z = cameraHeight;
-			
-			var rMousePos = Camera.main.ScreenToWorldPoint(mousePos) - transform.position;
-			var cMousePos = cardinalize (rMousePos);
-			if (Vector3.Distance(rMousePos, Vector3.zero) >= 1) 
+			Vector3 rMousePos = relativeMousePosition();
+				
+			if (Vector3.Distance(rMousePos, Vector3.zero) > 1 && atTargetPos)
 			{
-				targetPos = roundedVec(transform.position + cardinalize(rMousePos));
+				targetPos = intVector3(transform.position + vecToGridPlane(rMousePos));
+				atTargetPos = false;
 			}
 		}
-		
-		Vector3 targetDir = cardinalize (targetPos - transform.position);
-		bool shouldMove = !Physics.Raycast (transform.position, targetDir, 0.5f);
+
+		Vector3 targetDir = vecToGridPlane(targetPos - transform.position);
+		bool shouldMove = !Physics.Raycast(transform.position + targetDir, targetDir, 0.5f);
 		if (shouldMove) {
-			transform.position = Vector3.MoveTowards (transform.position, targetPos, speed * Time.deltaTime);
+			Vector3 newPos = Vector3.MoveTowards(transform.position, targetPos, gSpeed * Time.deltaTime);
+			transform.position = newPos;
+			atTargetPos = newPos == targetPos;
 		}
 	}
 
-	Vector3 cardinalize (Vector3 vector) {
-		Vector3 cv = Vector3.zero;
-		float ax = Mathf.Abs (vector.x);
-		float az = Mathf.Abs (vector.z);
-		if (ax > az) {
-			cv.x = vector.x/ax;
-		} else {
-			cv.z = vector.z/az;
+	Vector3 relativeMousePosition() {
+		// NOTE: assuming the camera starts directly above the player
+		float cameraHeight = Mathf.Abs(Vector3.Distance(gMainCamera.transform.position, transform.position));
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = cameraHeight;
+		return Camera.main.ScreenToWorldPoint(mousePos) - transform.position;
+	}
+
+	Vector3 vecToGridPlane(Vector3 vector) {
+		Vector3 v = Vector3.zero;
+		float ax = Mathf.Abs(vector.x);
+		float ay = Mathf.Abs(vector.y);
+		float az = Mathf.Abs(vector.z);
+		
+		if (gNormal == Vector3.down || gNormal == Vector3.up) {	
+			if (ax > az) {
+				v.x = vector.x/ax;
+			} else {
+				v.z = vector.z/az;
+			}
 		}
-		return cv;
+		else if (gNormal == Vector3.left || gNormal == Vector3.right) {
+			if (ay > az) {
+				v.y = vector.y/ax;
+			} else {
+				v.z = vector.z/az;
+			}
+		}
+		else if (gNormal == Vector3.forward || gNormal == Vector3.back) {
+			if (ax > ay) {
+				v.x = vector.x/ax;
+			} else {
+				v.y = vector.y/ay;
+			}	
+		}
+		return v;
 	}
-	
-	// TODO learn about vector math
-	// TODO move this somewhere shared
-	Vector3 roundedVec (Vector3 v) {
-		return new Vector3 (Mathf.RoundToInt (v.x),
-		                    0,
-		                    Mathf.RoundToInt (v.z));
+
+	Vector3 intVector3(Vector3 v) {
+		int x = Mathf.RoundToInt(v.x);
+		int y = Mathf.RoundToInt(v.y);
+		int z = Mathf.RoundToInt(v.z);
+		if (gNormal == Vector3.down || gNormal == Vector3.up) {	
+			y = 0;
+		}
+		else if (gNormal == Vector3.left || gNormal == Vector3.right) {
+			x = 0;		
+		}
+		else if (gNormal == Vector3.forward || gNormal == Vector3.back) {
+			z = 0;
+		}
+		return new Vector3 (x, y, z);
 	}
-	
+
 }
