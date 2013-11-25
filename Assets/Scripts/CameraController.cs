51fdbb21aa778197fa3c4de5c8e1f54ebee7a8e4
Ben Guo <ben@venmo.com>
2013-11-24 23:57:14 -0500

    adds cameracontroller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	Vector3 POSITION_LEFT = new Vector3(-10.0f, 0.0f, 0.0f); 
+	Quaternion ROTATION_LEFT = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+
+	Vector3 POSITION_UP = new Vector3(0.0f, 10.0f, 0.0f);
+	Quaternion ROTATION_UP = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+
+	Vector3 targetPosition;
+	Quaternion targetRotation;
+
+	void Start () {
+		targetPosition = POSITION_UP;
+		targetRotation = ROTATION_UP;
+	}
+	
+	void LateUpdate () {
+		transform.position = player.transform.position + targetPosition;
+//		transform.position = targetPosition;
+		transform.rotation = targetRotation;	
+	}
+	
+	public void RotateLeft () {
+		targetPosition = POSITION_LEFT;
+		targetRotation = ROTATION_LEFT;
+	}
+	
+	public void RotateUp () {
+		targetPosition = POSITION_UP;
+		targetRotation = ROTATION_UP;
+	}
+}
