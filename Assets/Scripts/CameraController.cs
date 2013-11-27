f6a7392d2d5f517afac4773e12b1957007009438
Ben Guo <ben@venmo.com>
2013-11-27 13:07:12 -0500

    Adds rotations

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,103 +3,186 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-
+		// Camera position + screen up direction
+		// Back = Screen out
+		enum Orientations {
+				UpForward,
+				UpRight,
+				UpBack,
+				UpLeft,
+				ForwardUp,
+				ForwardRight,
+				ForwardDown,
+				ForwardLeft,
+				DownBack,
+				DownRight,
+				DownForward,
+				DownLeft,
+				BackUp,
+				BackLeft,
+				BackDown,
+				BackRight,
+				LeftUp,
+				LeftForward,
+				LeftDown,
+				LeftBack,
+				RightUp,
+				RightBack,
+				RightDown,
+				RightForward		
+		};
+		
+		Vector3 POSITION_UP = new Vector3 (0.0f, 10.0f, 0.0f);
+		Vector3 POSITION_FORWARD = new Vector3 (0.0f, 0.0f, 10.0f);
+		Vector3 POSITION_DOWN = new Vector3 (0.0f, -10.0f, 0.0f);
+		Vector3 POSITION_BACK = new Vector3 (0.0f, 0.0f, -10.0f);
+		Vector3 POSITION_LEFT = new Vector3 (-10.0f, 0.0f, 0.0f);
+		Vector3 POSITION_RIGHT = new Vector3 (10.0f, 0.0f, 0.0f);
+		
+		Quaternion ROTATION_UP_F = Quaternion.Euler (90.0f, 0.0f, 0.0f);
+		Quaternion ROTATION_UP_L = Quaternion.Euler (90.0f, 0.0f, 90.0f);
+		Quaternion ROTATION_UP_B = Quaternion.Euler (90.0f, 0.0f, 180.0f);
+		Quaternion ROTATION_UP_R = Quaternion.Euler (90.0f, 0.0f, 270.0f);
+		
+		Quaternion ROTATION_FORWARD_U = Quaternion.Euler (0.0f, 180.0f, 0.0f);
+		Quaternion ROTATION_FORWARD_R = Quaternion.Euler (0.0f, 180.0f, 90.0f);
+		Quaternion ROTATION_FORWARD_D = Quaternion.Euler (0.0f, 180.0f, 180.0f);
+		Quaternion ROTATION_FORWARD_L = Quaternion.Euler (0.0f, 180.0f, 270.0f);
+		
+		Quaternion ROTATION_DOWN_B = Quaternion.Euler (-90.0f, 0.0f, 0.0f);
+		Quaternion ROTATION_DOWN_L = Quaternion.Euler (-90.0f, 90.0f, 0.0f);	
+		Quaternion ROTATION_DOWN_F = Quaternion.Euler (-90.0f, 180.0f, 0.0f);	
+		Quaternion ROTATION_DOWN_R = Quaternion.Euler (-90.0f, 270.0f, 0.0f);	
+		
+		Quaternion ROTATION_BACK_U = Quaternion.Euler (0.0f, 0.0f, 0.0f);
+		Quaternion ROTATION_BACK_L = Quaternion.Euler (0.0f, 0.0f, 90.0f);	
+		Quaternion ROTATION_BACK_D = Quaternion.Euler (0.0f, 0.0f, 180.0f);	
+		Quaternion ROTATION_BACK_R = Quaternion.Euler (0.0f, 0.0f, 270.0f);	
+		
+		Quaternion ROTATION_LEFT_U = Quaternion.Euler (0.0f, 90.0f, 0.0f);
+		Quaternion ROTATION_LEFT_F = Quaternion.Euler (0.0f, 90.0f, 90.0f);
+		Quaternion ROTATION_LEFT_D = Quaternion.Euler (0.0f, 90.0f, 180.0f);
+		Quaternion ROTATION_LEFT_B = Quaternion.Euler (0.0f, 90.0f, 270.0f);
+		
+		Quaternion ROTATION_RIGHT_U = Quaternion.Euler (0.0f, 270.0f, 0.0f);
+		Quaternion ROTATION_RIGHT_B = Quaternion.Euler (0.0f, 270.0f, 90.0f);
+		Quaternion ROTATION_RIGHT_D = Quaternion.Euler (0.0f, 270.0f, 180.0f);
+		Quaternion ROTATION_RIGHT_F = Quaternion.Euler (0.0f, 270.0f, 270.0f);
+		
 		public GameObject player;
 		public float gRotationSpeed = 6.0f;
 		
-		Vector3 POSITION_BACK       = new Vector3 (0.0f, 0.0f, -10.0f);
-		Quaternion ROTATION_BACK    = Quaternion.Euler (0.0f, 0.0f, 0.0f);
-		Vector3 POSITION_DOWN_X     = new Vector3 (0.0f, -10.0f, 0.0f);				// LR
-		Quaternion ROTATION_DOWN_X  = Quaternion.Euler (-90.0f, 90.0f, 90.0f);
-		Vector3 POSITION_DOWN_Z     = new Vector3 (0.0f, -10.0f, 0.0f);				// BF
-		Quaternion ROTATION_DOWN_Z  = Quaternion.Euler (-90.0f, 0.0f, 0.0f);	
-		Vector3 POSITION_FORWARD    = new Vector3 (0.0f, 0.0f, 10.0f);
-		Quaternion ROTATION_FORWARD = Quaternion.Euler (0.0f, 180.0f, 180.0f);	
-		Vector3 POSITION_LEFT_Y     = new Vector3 (-10.0f, 0.0f, 0.0f);				// DU
-		Quaternion ROTATION_LEFT_Y  = Quaternion.Euler (0.0f, 90.0f, 90.0f);
-		Vector3 POSITION_LEFT_Z     = new Vector3 (-10.0f, 0.0f, 0.0f);				// BF
-		Quaternion ROTATION_LEFT_Z  = Quaternion.Euler (0.0f, 0.0f, 0.0f);	
-		Vector3 POSITION_RIGHT      = new Vector3 (10.0f, 0.0f, 0.0f);
-		Quaternion ROTATION_RIGHT   = Quaternion.Euler (0.0f, -90.0f, -90.0f);
-		Vector3 POSITION_UP         = new Vector3 (0.0f, 10.0f, 0.0f);
-		Quaternion ROTATION_UP      = Quaternion.Euler (90.0f, 0.0f, 0.0f);
-
 		Vector3 targetPosition;
 		Vector3 normal;
+		Orientations orientation;
 		Quaternion targetRotation;
 
 		void Start ()
 		{
-				normal = Vector3.up;
+				orientation = Orientations.UpForward;
 				targetPosition = POSITION_UP;
-				targetRotation = ROTATION_UP;
+				targetRotation = ROTATION_UP_F;
 		}
 	
 		void LateUpdate ()
 		{
-//		if (transform.position != targetPosition) {
 				transform.position = Vector3.Lerp (transform.position, 
 											   player.transform.position + targetPosition, 
 											   gRotationSpeed * Time.deltaTime);
-//		}
 				if (transform.rotation != targetRotation) {
 						transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, gRotationSpeed * Time.deltaTime);	
 				}
 		}
 	
-		public void RotateBack ()
+		public void RotateUp ()
 		{
-				targetPosition = POSITION_BACK;
-				targetRotation = ROTATION_BACK;
-				normal = Vector3.back;
-		}
-		
-		public void RotateDown ()
-		{
-				if (normal == Vector3.left || normal == Vector3.right) {
-						targetPosition = POSITION_DOWN_X;
-						targetRotation = ROTATION_DOWN_X;
-						normal = Vector3.down;
+				if (orientation == Orientations.LeftForward 
+				 || orientation == Orientations.RightForward
+				 || orientation == Orientations.BackUp 
+				 || orientation == Orientations.ForwardDown) {
+						targetRotation = ROTATION_UP_F;
+						orientation = Orientations.UpForward;	
 				}
-				else if (normal == Vector3.back || normal == Vector3.forward) {
-						targetPosition = POSITION_DOWN_Z;
-						targetRotation = ROTATION_DOWN_Z;
-						normal = Vector3.down;
-				}
+				else if (orientation == Orientations.LeftBack
+	  		          || orientation == Orientations.RightBack
+	  		          || orientation == Orientations.BackDown
+	  		          || orientation == Orientations.ForwardUp) {
+	  		    		targetRotation = ROTATION_UP_B;
+	  		    		orientation = Orientations.UpBack;      
+  		        }
+				else if (orientation == Orientations.ForwardLeft
+					  || orientation == Orientations.BackLeft 
+					  || orientation == Orientations.LeftUp
+					  || orientation == Orientations.RightDown) {
+						targetRotation = ROTATION_UP_L;
+						orientation = Orientations.UpLeft;	  
+			  	}
+			  	else if (orientation == Orientations.ForwardRight
+			  		  || orientation == Orientations.BackRight
+			  		  || orientation == Orientations.RightUp
+			  		  || orientation == Orientations.RightDown) {
+			  			targetRotation = ROTATION_UP_R;
+			  			orientation = Orientations.UpRight;	  
+	  		    }
+  		        else {
+  		        		return;
+  		        }
+				targetPosition = POSITION_UP;	
 		}
 		
 		public void RotateForward ()
 		{
-				targetPosition = POSITION_FORWARD;
-				targetRotation = ROTATION_FORWARD;
-				normal = Vector3.forward;
+				if (orientation == Orientations.UpForward
+				 || orientation == Orientations.RightForward
+				 || orientation == Orientations.LeftForward
+				 || orientation == Orientations.DownBack) {
+						targetRotation = ROTATION_FORWARD_D;
+						orientation = Orientations.ForwardDown; 
+				}
+				else if (orientation == Orientations.UpBack
+				      || orientation == Orientations.RightBack
+				      || orientation == Orientations.LeftBack
+				      || orientation == Orientations.DownBack) {
+				      	targetRotation = ROTATION_FORWARD_U;
+				      	orientation = Orientations.ForwardUp;
+		        }
+				else if (orientation == Orientations.UpLeft
+				      || orientation == Orientations.RightForward
+				      || orientation == Orientations.DownLeft
+				      || orientation == Orientations.LeftBack) {
+						targetRotation = ROTATION_FORWARD_L;
+						orientation = Orientations.ForwardLeft;      
+			    }
+			    else if (orientation == Orientations.UpRight
+			          || orientation == Orientations.RightBack
+			          || orientation == Orientations.DownRight
+			          || orientation == Orientations.LeftForward) {
+			    		targetRotation = ROTATION_FORWARD_R;
+			    		orientation = Orientations.ForwardRight;      
+	            }
+	            else {
+	            		return;
+	            }
+	            targetPosition = POSITION_FORWARD;
 		}
-	
+		
+		public void RotateDown ()
+		{
+
+		}	
+		
+		public void RotateBack ()
+		{
+
+		}
+			
 		public void RotateLeft ()
 		{
-				if (normal == Vector3.down || normal == Vector3.up) {
-						targetPosition = POSITION_LEFT_Y;
-						targetRotation = ROTATION_LEFT_Y;
-						normal = Vector3.left;
-				}
-				else if (normal == Vector3.back || normal == Vector3.forward) {
-						targetPosition = POSITION_LEFT_Z;
-						targetRotation = ROTATION_LEFT_Z;
-						normal = Vector3.left;
-				}
-		}
-	
-		public void RotateUp ()
-		{
-				targetPosition = POSITION_UP;
-				targetRotation = ROTATION_UP;
-				normal = Vector3.up;
+
 		}
 	
 		public void RotateRight ()
 		{
-				targetPosition = POSITION_RIGHT;
-				targetRotation = ROTATION_RIGHT;
-				normal = Vector3.right;
+
 		}
 }
